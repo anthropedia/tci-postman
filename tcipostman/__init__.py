3490bedad0bc79eccce4f2dae50d33353b8bfb2d
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-05-23 18:10:58 +0200

    send password to user when created

--- tcipostman/__init__.py
+++ tcipostman/__init__.py
@@ -8,6 +8,8 @@ def init_app(app):
     Postman.mail = Mail(app)
     db.signals.post_save.connect(Postman.send_client_token,
                                  sender=models.SurveyToken)
+    db.signals.post_save.connect(Postman.send_user_password,
+                                 sender=models.User)
 
 
 class Postman:
@@ -23,3 +25,17 @@ class Postman:
                         cls.app.config['TCI_TEST_URL'].format(
                             token=document.key)))
             cls.mail.send(msg)
+
+    @classmethod
+    def send_user_password(cls, sender, document, created):
+        email = document.email
+        if not created or not email:
+            return
+        password = document.generate_password()
+        msg = Message('Your TCI access', recipients=[email])
+        msg.body = ('''
+You can access the TCI on {url} with your email and
+the following password: {pwd}'''.format(
+            url=cls.app.config['TCI_PROFESSIONALS_URL'], pwd=password))
+        cls.mail.send(msg)
+        document.save()
